bb8f364b7ab77af3d3f79d2a68422b4522064555
Zlopyhatel <pismomne86@gmail.com>
2026-02-21 15:17:31 +0200

    Initialize commit

---
+++ Stepik.py
@@ -0,0 +1,21 @@
+"""""
+print("I'm", 'Just', 'Ken', "!")
+
+print('Как тебя зовут?')     # вывод текста
+print('Привет,', input())    # ввод текста и вывод текста
+
+variable_name = input()
+print('Вы ввели текст:', variable_name)
+
+name = input()
+print(name, "- чемпион!")
+
+"""""
+str1 = input()
+str2 = input()
+str3 = input()
+
+print(str3)
+print(str2)
+print(str1)
+
