445a9314dae10efd49f61ac6bde27c16701dfa3c
Zlopyhatel <pismomne86@gmail.com>
2026-02-23 18:49:14 +0200

    End of 23/02/2026

--- Stepik.py
+++ Stepik.py
@@ -10,7 +10,6 @@ print('Вы ввели текст:', variable_name)
 name = input()
 print(name, "- чемпион!")
 
-"""""
 str1 = input()
 str2 = input()
 str3 = input()
@@ -18,5 +17,77 @@ str3 = input()
 print(str3)
 print(str2)
 print(str1)
+#23/02/2026
+print('I','like','Python', sep='***')
+
+name = input()
+print('Привет,', name, end='!')
 
 
+sep = input()
+arg1 = input()
+arg2 = input()
+arg3 = input()
+print(arg1, arg2, arg3, sep=sep)
+
+x = int(input())
+print(x)
+print(x + 1)
+print(x + 2)
+
+x = int(input())
+y = int(input())
+z = int(input())
+w = int(input())
+print((x + y + z + w)*3)
+
+a = int(input())
+b = int(input())
+c = 3*(a+b)**3 + 275*b**2 - 127*a - 41
+print(c)
+
+x = int(input())
+print('Следующее за числом',x, 'число:',x+1)
+print('Для числа',x, 'предыдущее число:',x-1)
+
+a = int(input())
+print('Объем =',a**3)
+print('Площадь полной поверхности =',6*a**2)
+
+a = int(input())
+b = int(input())
+print(a, '+', b, '=', a + b)
+print(a, '-', b, '=', a - b)
+print(a, '*', b, '=', a * b)
+
+a1 = int(input())
+d = int(input())
+n = int(input())
+print(a1 + d*(n-1))
+
+x = int(input())
+print(x, x*2, x*3, x*4, x*5, sep='---')
+
+a = 15 // (16 % 7)
+b = 34 % a * 5 - 29 % 5 * 2
+print(a + b)
+
+a = 82 // 3**2 % 7
+print(a)
+
+b = int(input())
+q = int(input())
+n = int(input())
+print(b * q**(n-1))
+
+cm = int(input())
+print(cm // 100)
+
+n = int(input()) #students
+k = int(input()) #mandarin
+print(k // n)
+print(k % n)
+
+x = int(input())
+print(-(-x // 2))
+"""""
