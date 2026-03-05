43c944e43c89751019f08c6fc121ad003fc62f84
Zlopyhatel <pismomne86@gmail.com>
2026-03-05 15:56:03 +0300

    05/03/2026

--- Stepik.py
+++ Stepik.py
@@ -171,4 +171,136 @@ n = int(input())
 print(n + int(str(n)+str(n)) + int(str(n)+str(n)+str(n)))
 n = input()
 print(int(n)+int(n+n)+int(n+n+n))
+
+#4  Условный оператор
+word = input()
+if word == 'Python':
+    print('ДА')
+else:
+    print('НЕТ')
+#https://stepik.org/lesson/265081/step/12?unit=246029
+str1 = input()
+str2 = input()
+if str1 == str2:
+    print('Пароль принят')
+else:
+    print('Пароль не принят')
+
+#https://stepik.org/lesson/265081/step/13?unit=246029
+n = int(input())
+if n%2 != 0:
+    print('Нечетное')
+else:
+    print('Четное')
+
+#https://stepik.org/lesson/265081/step/14?unit=246029
+n = int(input())
+if n >= 18:
+    print('Доступ разрешен')
+else:
+    print('Доступ запрещен')
+
+#https://stepik.org/lesson/265081/step/15?unit=246029
+a = int(input())
+b = int(input())
+if a > b:
+    print(b)
+else:
+    print(a)
+
+#05/03/2026
+#https://stepik.org/lesson/265081/step/16?unit=246029
+a = int(input())
+b = int(input())
+c = int(input())
+if c-b == b-a and c == b + (b-a):
+    print('YES')
+else:
+    print('NO')
+
+#https://stepik.org/lesson/265081/step/17?unit=246029
+#сумма первой и последней цифр равна разности второй и третьей цифр.
+num=int(input())
+digit4 = num % 10
+digit3 = (num // 10) % 10
+digit2 = (num // 100) % 10
+digit1 = num // 1000
+if (digit1 + digit4) == (digit2 - digit3):
+    print('ДА')
+else:
+    print('НЕТ')
+
+#https://stepik.org/lesson/265081/step/18?unit=246029
+#считывает три числа и подсчитывает сумму только положительных чисел
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+sum_positive = 0
+if num1 > 0:
+    sum_positive += num1
+if num2 > 0:
+    sum_positive += num2
+if num3 > 0:
+    sum_positive += num3
+print(sum_positive)
+
+#https://stepik.org/lesson/265081/step/19?unit=246029
+#по введённому возрасту пользователя сообщает, к какой возрастной группе он относится
+num = int(input())
+if num <= 13:
+    print('детство')
+if 14 <= num <= 24:
+    print('молодость')
+if 25 <= num <= 59:
+    print('зрелость')
+if num >= 60:
+    print('старость')
+
+#https://stepik.org/lesson/265081/step/20?unit=246029
+#которая определяет наименьшее из четырёх чисел
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+num = num1
+if num > num2:
+    num = num2
+if num > num3:
+    num = num3
+if num > num4:
+    num = num4
+print(num)
+
+#examples
+#https://stepik.org/lesson/265083/step/2?unit=246031
+#Напишите программу, которая определяет, является ли заданное натуральное число трёхзначным.
+num = int(input())
+if 100 <= num <= 999:     # num >= 100 and num <= 999
+    print('Число является трёхзначным')
+else:
+    print('Число не является трёхзначным')
+    
+#Напишите программу, которая проверяет, что все три цифры натурального трёхзначного числа различны
+num = int(input())
+d3 = num % 10
+d2 = num % 100 // 10
+d1 = num // 100
+if d3 != d2 and d3 != d1 and d2 != d1:
+    print('Цифры различны')
+else:
+    print('Цифры не различны')
+    
+#Напишите программу, которая по координатам точки, не лежащей на осях координат,
+# определяет номер координатной четверти, в которой она находится.
+x = int(input())
+y = int(input())
+if x > 0 and y > 0:
+    print('1 четверть')
+if x < 0 and y > 0:
+    print('2 четверть')
+if x < 0 and y < 0:
+    print('3 четверть')
+if x > 0 and y < 0:
+    print('4 четверть')
+
 """""
