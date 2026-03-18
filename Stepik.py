6dd2cf09e81e2c02d33213f55601574a7cf75ef7
Zlopyhatel <pismomne86@gmail.com>
2026-03-18 15:35:54 +0300

    18/03/2026

--- Stepik.py
+++ Stepik.py
@@ -1029,7 +1029,8 @@ elif m < n:
         print(i)
 else:
     print(m)
-"""""
+
+#18/03/2026
 counter = 0                                             # создаём переменную счётчика
 for _ in range(10):
     num = int(input())
@@ -1037,3 +1038,52 @@ for _ in range(10):
         counter = counter + 1                           # увеличиваем значение cчётчика
 
 print('Было введено', counter, 'чисел, больших 10.')
+
+counter1 = 0
+counter2 = 0
+for _ in range(10):
+    num = int(input())
+    if num > 10:
+        counter1 = counter1 + 1
+    if num == 0:
+        counter2 = counter2 + 1
+print('Было введено', counter1, 'чисел, больших 10.')
+print('Было введено', counter2, 'нулей.' )
+
+total = 0
+for _ in range(10):
+    num = int(input())
+    if num > 10:
+        total = total + num
+print('Сумма чисел больших 10 равна',  total)
+
+total = 0
+for _ in range(10):
+    num = int(input())
+    total = total + num
+average = total / 10
+print('Среднее значение равно', average)
+
+total_len = 0
+for _ in range(3):
+    word = input()
+    total_len = total_len + len(word)
+print(total_len)
+
+#Сириус
+#Кассиопея
+#Каллисто
+
+"""""
+total = 0
+for i in range(1, 6):
+    total += i
+    print(total, end='')
+
+flag = False
+for _ in range(5):
+    word = input()
+    if len(word) < 5:
+        flag = True
+print(flag)
+
